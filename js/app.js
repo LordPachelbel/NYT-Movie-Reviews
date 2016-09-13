e987c027815d4818795eb274fc80c522f2364612
Jordan Bradford <LordPachelbel@users.noreply.github.com>
2016-09-13 00:24:43 -0500

    Added jquery-template.js and started making results template

--- js/app.js
+++ js/app.js
@@ -2,21 +2,50 @@ jQuery(function($) {
 
 	$("#search-form").submit(function(e) {
 		e.preventDefault();
-		var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
-		url += '?' + $.param({
-			'api-key': API_KEY_MOVIES
-		});
-		$.ajax({
-			url: url,
-			method: 'GET',
+
+		var endpoint = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+
+		$.getJSON(endpoint, {
+			'api-key': API_KEY_MOVIES,
+			'query': 'Jaws'
 		}).done(function(result) {
 			console.log(result);
-			$("#results").text(JSON.stringify(result));
+			//$("#results").text(JSON.stringify(result));
+
+			var items = [];
+			$.each(result.results, function(key, movieReviewObject) {
+				items.push( "<li id='" + key + "'>" + movieReviewObject + "</li>" );
+			});
+			$( "<ul/>", {
+				"class": "my-new-list",
+				html: items.join("")
+			}).appendTo("#results");
 		}).fail(function(err) {
 			throw err;
 		});
+		
 		return false;
+
 	});
-
-
+	
 });
+
+
+
+
+
+/*$("#results").loadTemplate("templates/review.html",
+    {
+        author: 'Joe Bloggs',
+        date: '25th May 2013',
+        authorPicture: 'Authors/JoeBloggs.jpg',
+        post: 'This is the contents of my post'
+    },
+		{overwriteCache: true}
+		);
+});*/
+	
+	
+	
+
+
