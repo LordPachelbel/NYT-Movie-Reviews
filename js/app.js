835b5872c46f9815a9a62f3f89d505b75b0c2ef9
Jordan Bradford <LordPachelbel@users.noreply.github.com>
2016-09-13 23:19:57 -0500

    Working on the pager

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 jQuery(function($) {
 
+  var currentOffset = 0;
+  var hasMoreReviews = false;
+  $(".reviews-footer").hide();
+
   $.addTemplateFormatter({
     datePrettyFormatter: function(value, template) {
       return new Date(value).toDateString();
@@ -19,10 +23,17 @@ jQuery(function($) {
     }
   });
 
-
   $("#search-form").submit(function(e) {
     e.preventDefault();
+    //console.log(e);
 
+    if(e.isTrigger === null) {
+      console.log("normal submission");
+    } else {
+      console.log("pager submission");
+    }
+    
+    
     var endpoint = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
 
     var data = $.param({
@@ -34,7 +45,7 @@ jQuery(function($) {
     $.getJSON(endpoint, data).done(function(data) {
       console.log(data);
 
-      // todo - check data.has_more=true to repeatedly call the API to get all results
+      hasMoreReviews = data.has_more;
       
       if(data.num_results == 0) {
         $("#results").html("<p>No results found.</p>")
@@ -57,14 +68,58 @@ jQuery(function($) {
 
       });
 
-      $("#results").loadTemplate("templates/review.html", data.results, {isFile: true});
+      $("#results").loadTemplate("templates/review.html", data.results, {
+        isFile: true,
+        success: addPager(hasMoreReviews, currentOffset),
+        bindingOptions: {
+          ignoreUndefined: true,
+          ignoreNull: true,
+          ignoreEmptyString: true
+        }
+      });
 
     }).fail(function(err) {
-      throw err;
+      console.log(err);
     });
     
     return false;
 
   });
 
+  $(".next").click(function(e) {
+    e.preventDefault();
+
+    currentOffset += 20;
+    console.log(currentOffset);
+    $("#search-form").submit();
+
+    return false;
+  });
+
+  $(".previous").click(function(e) {
+    e.preventDefault();
+
+    currentOffset -= 20;
+    console.log(currentOffset);
+    $("#search-form").submit();
+
+    return false;
+  });
+
 });
+
+function addPager(hasMoreReviews, currentOffset) {
+  if(hasMoreReviews) {
+    $(".next").removeClass("disabled");
+  } else {
+    $(".next").addClass("disabled");
+  }
+
+  if(currentOffset > 0) {
+    $(".previous").removeClass("disabled");
+  } else {
+    $(".previous").addClass("disabled");
+  }
+
+  $(".reviews-footer").show();
+}
