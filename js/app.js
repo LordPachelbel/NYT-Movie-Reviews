8e2ea19e9040f47728f0bde4f7d049f66e48271c
Jordan Bradford <LordPachelbel@users.noreply.github.com>
2016-09-13 21:32:48 -0500

    Converted tabs to spaces

--- js/app.js
+++ js/app.js
@@ -1,70 +1,70 @@
 jQuery(function($) {
 
-	$.addTemplateFormatter({
-		datePrettyFormatter: function(value, template) {
-			return new Date(value).toDateString();
-		},
-		upperCaseFormatter: function(value, template) {
-			return value.toUpperCase();
-		},
-		lowerCaseFormatter: function(value, template) {
-			return value.toLowerCase();
-		},
-		sameCaseFormatter: function(value, template) {
-			if(template == "upper") {
-				return value.toUpperCase();
-			} else {
-				return value.toLowerCase();
-			}
-		}
-	});
+  $.addTemplateFormatter({
+    datePrettyFormatter: function(value, template) {
+      return new Date(value).toDateString();
+    },
+    upperCaseFormatter: function(value, template) {
+      return value.toUpperCase();
+    },
+    lowerCaseFormatter: function(value, template) {
+      return value.toLowerCase();
+    },
+    sameCaseFormatter: function(value, template) {
+      if(template == "upper") {
+        return value.toUpperCase();
+      } else {
+        return value.toLowerCase();
+      }
+    }
+  });
 
 
-	$("#search-form").submit(function(e) {
-		e.preventDefault();
+  $("#search-form").submit(function(e) {
+    e.preventDefault();
 
-		var endpoint = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+    var endpoint = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
 
-		var data = $.param({
-			'api-key': API_KEY_MOVIES,
-			'order': 'by-title'
-		}) + '&' + $(this).serialize();
-		//console.log(data);
+    var data = $.param({
+      'api-key': API_KEY_MOVIES,
+      'order': 'by-title'
+    }) + '&' + $(this).serialize();
+    //console.log(data);
 
-		$.getJSON(endpoint, data).done(function(data) {
-			console.log(data);
+    $.getJSON(endpoint, data).done(function(data) {
+      console.log(data);
 
-			// todo - check data.has_more=true to repeatedly call the API to get all results
-			
-			if(data.num_results == 0) {
-				$("#results").html("<p>No results found.</p>")
-				return false;
-			}
+      // todo - check data.has_more=true to repeatedly call the API to get all results
+      
+      if(data.num_results == 0) {
+        $("#results").html("<p>No results found.</p>")
+        return false;
+      }
 
-		  $.each(data.results, function(key, review) {
+      $.each(data.results, function(key, review) {
 
-				templateData = null;
-				templateData = {
-					reviewLink: review.link.url,
-					reviewTitle: review.link.suggested_link_text,
-					reviewAlt: review.link.suggested_link_text,
-					reviewImage: (review.multimedia !== null) ? review.multimedia.src : 'images/no-image.gif',
-					criticsPick: (review.critics_pick == 1) ? '<span class="glyphicon glyphicon-star" title="NYT Critics\' Pick"></span>' : ''
-				};
+        templateData = null;
+        templateData = {
+          reviewLink: review.link.url,
+          reviewTitle: review.link.suggested_link_text,
+          reviewAlt: review.link.suggested_link_text,
+          reviewImage: (review.multimedia !== null) ? review.multimedia.src : 'images/no-image.gif',
+          criticsPick: (review.critics_pick == 1) ? '<span class="glyphicon glyphicon-star" title="NYT Critics\' Pick"></span>' : ''
+        };
 
-				$.extend(review, templateData);
-				//console.log(review);
+        $.extend(review, templateData);
+        //console.log(review);
 
-			});
+      });
 
-			$("#results").loadTemplate("templates/review.html", data.results, {isFile: true});
+      $("#results").loadTemplate("templates/review.html", data.results, {isFile: true});
 
-		}).fail(function(err) {
-			throw err;
-		});
-		
-		return false;
+    }).fail(function(err) {
+      throw err;
+    });
+    
+    return false;
 
-	});
+  });
 
 });
--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 jQuery(function($) {
-	// make external links open in a new window
+  // make external links open in a new window
   $("a[rel~='external']").click(function() {
     window.open(this.href);
     return false;
