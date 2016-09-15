48a11e6c3b1f7527f4cbe3517c054fd415e7a5bb
Jordan Bradford <LordPachelbel@users.noreply.github.com>
2016-09-14 23:20:04 -0500

    Query objects now build their query strings completely

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
+// Note: The API is messed up somehow because date-based queries don't return correct results, even if the queries are run from the API console on the developer website (http://developer.nytimes.com)
+// For example, go to http://developer.nytimes.com/movie_reviews_v2.json#/Console/GET/reviews/search.json and put one of the following values in the publication-date field:
+// 2012-05-30
+// 2012-05-30;2012-05-31
+// No matter what date[s] you enter, you're going to get results showing their latest reviews in descending chronological order.
+// Because of this, I have not added UI elements or JavaScript code to handle searching for reviews by date
+
+
 // NYT Movie Reviews API constants
 var baseURL = "https://api.nytimes.com/svc/movies/v2/";
 var endpoints = {
-  allReviews:         "reviews/all.json",       // All reviews
-  picksInTheaters:    "reviews/picks.json",     // NYT Critics' Picks currently in theaters
-  keywordSearch:      "reviews/search.json",    // Keyword search
-  allCritics:         "critics/all.json",       // All Times reviewers
-  allFullTimeCritics: "critics/full-time.json", // All full-time Times reviewers
-  allPartTimeCritics: "critics/part-time.json", // All part-time Times reviewers
-  specificCritic:     "critics/"                // A specific critic. You have to complete the string like this: "critics/[reviewer name].json"
-                                                // ^ For example, "critics/Stephen Holden.json"
+  keywordSearch:          "reviews/search.json",    // search by keyword
+  publicationDateSearch:  "reviews/search.json",    // search by publication date; uses the same endpoint as the keyword search
+  allReviews:             "reviews/all.json",       // get all reviews
+  picksInTheaters:        "reviews/picks.json",     // get all NYT Critics' Picks currently in theaters
+  allCritics:             "critics/all.json",       // get all Times reviewers
+  allFullTimeCritics:     "critics/full-time.json", // get all full-time Times reviewers
+  allPartTimeCritics:     "critics/part-time.json", // get all part-time Times reviewers
+  specificCritic:         "critics/"                // a specific critic. You have to complete the string like this: "critics/[reviewer name].json"
+                                                    // ^ for example, "critics/Stephen Holden.json"
 }
 
 var currentOffset = 0;        // The API returns results 20 at a time.
 var hasMoreReviews = false;   // The API also tells you if you can get more reviews by adding 20 to the last offset value you used.
 
 
-// A bunch of objects and prototypes to handle the API endpoints and query parameters. I also felt like documenting these JSDoc-style, just because.
+// A bunch of objects and prototypes to handle the API endpoints and query parameters.
+// I also felt like documenting these JSDoc-style, just because.
 
 /**
  * API query prototype. Don't invoke it directly.
@@ -26,10 +36,15 @@ var hasMoreReviews = false;   // The API also tells you if you can get more revi
  * @param {('by-title'|'by-publication-date'|'by-opening-date')} order - The sort order of the results.
  */
 function reviewQueryObj(endpoint, offset, order) {
-  this.baseURL = baseURL;     // global variable
+  this.apiKey = API_KEY_MOVIES; // defined in a different file
+  this.baseURL = baseURL;       // global variable
   this.endpoint = endpoint;
   this.offset = offset;
   this.order = order;
+  this.queryString =
+    'api-key=' + this.apiKey +
+    '&offset=' + this.offset +
+    '&order='  + this.order;
 }
 
 /**
@@ -52,21 +67,59 @@ allReviewsQueryObj.prototype = new reviewQueryObj();
  * @param {('by-title'|'by-publication-date'|'by-opening-date')} order - The sort order of the results.
  */
 function allPicksReviewsObj(offset, order) {
-  reviewQueryObj.call(this, endpoints.picksInTheaters, offset, order); // endpoints is a global variable
+  reviewQueryObj.call(this, endpoints.picksInTheaters, offset, order);
 }
 allPicksReviewsObj.prototype = new reviewQueryObj();
 
+/**
+ * Query object prototype for getting reviews by keyword
+ *
+ * @class
+ * @param {number} offset - Positive integer, multiple of 20.
+ * @param {('by-title'|'by-publication-date'|'by-opening-date')} order - The sort order of the results.
+ * @param (string) keywords - The keyword[s] to search for.
+ * @param {(Y|N)} criticsPicks - (Optional) Limits the results based on whether the movie was (Y) or was not (N) a NYT Critics' Pick.
+ * @param (string) reviewer - (Optional) A reviewer's name. Limits the results to that critic's reviews.
+ */
+function keywordQueryObj(offset, order, keywords, criticsPicks, reviewer) { // I suppose the last two arguments could have been an object called "options"
+  reviewQueryObj.call(this, endpoints.keywordSearch, offset, order);
+
+  this.queryString += '&query=' + keywords;
+
+  if(criticsPicks) {
+    this.queryString += '&critics-pick=' + criticsPicks;
+  }
+
+  if(reviewer) {
+    this.queryString += '&reviewer=' + reviewer;
+  }
+}
+
+
+
+// Here is where I'd define query objects for date-based searches, but the API is broken. See the note at the top of this file.
+
+
+
+
+// Some query objects for testing purposes
+/*
 // query object for getting all reviews
-var allReviewsQuery = new allReviewsQueryObj(currentOffset, 'by-title');
-console.log(allReviewsQuery);
+var allReviewsQueryTest = new allReviewsQueryObj(currentOffset, 'by-title');
+console.log(allReviewsQueryTest);
 
 // query object for getting all NYT Critics' Picks currently in theaters
-var allPicksReviews = new allPicksReviewsObj(currentOffset, 'by-title');
-console.log(allPicksReviews);
+var allPicksReviewsTest = new allPicksReviewsObj(currentOffset, 'by-title');
+console.log(allPicksReviewsTest);
 
-
-
-
+// query object for getting reviews by keyword
+var keywordQueryTest1 = new keywordQueryObj(currentOffset, 'by-title', 'these are my keywords', '', 'A. O. Scott');
+var keywordQueryTest2 = new keywordQueryObj(currentOffset, 'by-title', 'these are different keywords', 'Y');
+var keywordQueryTest3 = new keywordQueryObj(currentOffset, 'by-title', 'random words');
+console.log(keywordQueryTest1);
+console.log(keywordQueryTest2);
+console.log(keywordQueryTest3);
+*/
 
 
 
@@ -185,10 +238,10 @@ function getReviewsFromAPI(searchForm) {
 
   var data = $.param({
     'api-key': API_KEY_MOVIES,
-    'order': 'by-title',
-    'offset': currentOffset
+    'offset': currentOffset,
+    'order': 'by-title'
   }) + '&' + searchForm.serialize();
-  //console.log(data);
+  console.log(data);
 
   $.getJSON(endpoint, data).done(function(data) {
     console.log(data);
